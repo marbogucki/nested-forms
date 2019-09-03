8ce4045921e6a7d44709a295b3d999fe9abd87ef
Marcin Bogucki <mbogucki@blastlab.co>
2019-09-03 13:30:14 +0200

    add validation

--- src/app/components/dynamic-form/dynamic-form.component.ts
+++ src/app/components/dynamic-form/dynamic-form.component.ts
@@ -1,5 +1,5 @@
 import {Component, Input, OnInit} from '@angular/core';
-import {FormBuilder, FormGroup} from '@angular/forms';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import {PhoneData} from '../../services/phones.service';
 
 @Component({
@@ -21,10 +21,33 @@ export class DynamicFormComponent implements OnInit {
   createGroup() {
     const group = this.fb.group({});
     this.phoneConfig.config.forEach(field => {
-      const control = this.fb.control(field.value);
+      const control = this.fb.control(field.value, this.bindValidations(field.validations || []));
       group.addControl(field.name, control);
     });
     return group;
+  }
+
+  bindValidations(validations: any) {
+    if (validations.length > 0) {
+      const validList = [];
+      validations.forEach(valid => {
+        if (valid.name === 'pattern') {
+          validList.push(Validators.pattern(valid.validator));
+        } else {
+          validList.push(Validators[valid.validator]);
+        }
+      });
+      console.log(Validators.compose(validList));
+      return Validators.compose(validList);
+    }
+    return null;
+  }
+
+  validateAllFormFields(formGroup: FormGroup) {
+    Object.keys(formGroup.controls).forEach(field => {
+      const control = formGroup.get(field);
+      control.markAsTouched({ onlySelf: true });
+    });
   }
 
 }
